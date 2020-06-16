cbbf500307b6a0b113a39234c5d92d72e3a66bff
hanulnun <hanulnun@gmail.com>
2020-06-16 23:25:43 +0900

    웹 크롤링과 텔레그램 봇

---
+++ detect_gift_card/detect_happy_money.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+from urllib import request
+import telegram
+
+# url = "http://www.11st.co.kr/product/SellerProductDetail.tmall?method=getSellerProductDetail&prdNo=2778024489"
+# req = request.urlopen(url)
+# res = req.read()
+#
+# soup = BeautifulSoup(res, 'html.parser')
+# keywords = soup.find_all('p', class_='info_guide')
+# keywords2 = soup.find_all('strong', class_='sale_price')
+# print(keywords)
+# print(keywords2)
+#
+# url2 = "http://www.11st.co.kr/product/SellerProductDetail.tmall?method=getSellerProductDetail&prdNo=2827952509"
+# req = request.urlopen(url2)
+# res = req.read()
+#
+# soup = BeautifulSoup(res, 'html.parser')
+# keywords = soup.find_all('p', class_='info_guide')
+# keywords2 = soup.find_all('strong', class_='sale_price')
+# print(keywords)
+# print(keywords2)
+
+telegram_token = '1206664250:AAFiDuoarJacXgn5NNZ_5N8Q9YyFv0sE5vE'
+bot = telegram.Bot(token=telegram_token)
+#8427228
+bot.sendMessage(chat_id='8427228', text="http://www.11st.co.kr/product/SellerProductDetail.tmall?method=getSellerProductDetail&prdNo=2778024489")
+
+
