15048f2ca15564ae16c95091db47d86cfa9204de
hanulnun <hanulnun@gmail.com>
2020-06-22 22:08:28 +0900

    웹 크롤링과 텔레그램 봇

--- detect_gift_card/detect_happy_money.py
+++ detect_gift_card/detect_happy_money.py
@@ -2,15 +2,15 @@ from bs4 import BeautifulSoup
 from urllib import request
 import telegram
 
-# url = "http://www.11st.co.kr/product/SellerProductDetail.tmall?method=getSellerProductDetail&prdNo=2778024489"
-# req = request.urlopen(url)
-# res = req.read()
-#
-# soup = BeautifulSoup(res, 'html.parser')
-# keywords = soup.find_all('p', class_='info_guide')
-# keywords2 = soup.find_all('strong', class_='sale_price')
-# print(keywords)
-# print(keywords2)
+url = "http://www.11st.co.kr/product/SellerProductDetail.tmall?method=getSellerProductDetail&prdNo=2778024489"
+req = request.urlopen(url)
+res = req.read()
+
+soup = BeautifulSoup(res, 'html.parser')
+keywords = soup.find_all('p', class_='info_guide')
+keywords2 = soup.find_all('strong', class_='sale_price')
+print(keywords)
+print(keywords2)
 #
 # url2 = "http://www.11st.co.kr/product/SellerProductDetail.tmall?method=getSellerProductDetail&prdNo=2827952509"
 # req = request.urlopen(url2)
